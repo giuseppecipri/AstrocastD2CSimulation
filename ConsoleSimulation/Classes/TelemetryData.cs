84496d16d1c47fc9810ab5709d7cf61b697cb147
Giuseppe <peppone.gc@gmail.com>
2018-07-29 14:18:06 +0200

    Improve and add App.config settings

--- ConsoleSimulation/Classes/TelemetryData.cs
+++ ConsoleSimulation/Classes/TelemetryData.cs
@@ -5,14 +5,14 @@ namespace ConsoleSimulation
 {
     class TelemetryData
     {
-        public int protocolVersion { get; set; }
         public string deviceId { get; set; }
+        public DateTime sendingDateTime { get; set; }
+        public int protocolVersion { get; set; }
         public string messageData { get; set; }
-        public double temperature { get; set; }
-        public double humidity { get; set; }
         public string pointInfo { get; set; }
         public string level { get; set; }
-        public DateTime sendingDateTime { get; set; }
+        public double temperature { get; set; }
+        public double humidity { get; set; }
 
         // Empty constructor
         public TelemetryData() { }
@@ -22,16 +22,19 @@ namespace ConsoleSimulation
         {
             this.protocolVersion = protocolVersion;
             this.deviceId = deviceId;
-            // set random values for temperature, humidity, level and pointInfo
-            SetRandomValue();
 
+            // set random values for temperature, humidity, level and pointInfo
+            SetRandomValues();
+
+            // set body of message
             messageData = Convert.ToBase64String(ASCIIEncoding.UTF8.GetBytes(pointInfo));
-                    
+            
+            // set sending date-time in universal time format
             sendingDateTime = DateTime.UtcNow;
         }
 
         // set random values for temperature, humidity, level and pointInfo
-        private void SetRandomValue()
+        private void SetRandomValues()
         {
             double minTemperature = 20;
             double minHumidity = 60;
@@ -40,8 +43,7 @@ namespace ConsoleSimulation
             temperature = minTemperature + rand.NextDouble() * 15;
             humidity = minHumidity + rand.NextDouble() * 20;
 
-
-            if (rand.NextDouble() > 0.75)
+            if (rand.NextDouble() > 0.8)
             {
                 if (rand.NextDouble() > 0.5)
                 {
@@ -59,7 +61,6 @@ namespace ConsoleSimulation
                 level = "normal";
                 pointInfo = "This is a normal message.";
             }
-
         }
     }
 }
